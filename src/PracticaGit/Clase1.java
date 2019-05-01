2271f374cfd47ad0e0de17f8079a4e7e99cd5c86
JuanAF <nombre@dominio.com>
2019-05-01 20:33:59 +0200

    Primer commit

---
+++ src/PracticaGit/Clase1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PracticaGit;
+
+/**
+ * @author Juan Asensi
+ * @version 1.0
+ * @date 1 may. 2019
+ */
+public class Clase1 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+
+}
